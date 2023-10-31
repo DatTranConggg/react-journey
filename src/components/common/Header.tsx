64fed8b39580abb1f623368c735b8a89029f46bc
Dat Tran <khiemtran21012000@gmail.com>
2023-10-31 14:32:14 +0700

    :pushpin: : add code to set project with index.ts file

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+// tsdrpfc 
+
+
+export interface HeaderProps {
+}
+
+export function Header (props: HeaderProps) {
+  return (
+    <div>
+      Header
+    </div>
+  );
+}
---
+++ src/features/labs/Student.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface StudentProps {
+}
+
+export default function Student (props: StudentProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
