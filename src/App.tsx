f8a19fc11c146bc3af39c9fbdcb51d2484d41b46
Dat Tran <khiemtran21012000@gmail.com>
2023-12-06 13:55:23 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import { StudentModel } from 'models'
 import './App.css'
-import { Student } from 'features/labs/Student'
-import MainLayout from 'layout/MainLayout'
-import { Gallery } from 'features/labs/Gallery'
-import { InputIssue } from 'features/labs/InputIssue'
-import FeedbackForm from 'features/labs/FeedbackForm'
+
 
 function App() {
-  const john: StudentModel = {
-    name: 'John',
-    age: 22,
-    isHero: true,
-    hobbyList: 'nhau',
-  }
 
-  const noo: StudentModel = {
-    name: 'Noo',
-    age: 22,
-    isHero: true,
-    hobbyList: 'nhau',
-  }
 
-  function handleOnClick(name: string) {
-    console.log('something !!!', name)
-  }
-
-  function testHandleClick() {
-    console.log('testHanleClick !!!')
-  }
 
   return (
     <>
