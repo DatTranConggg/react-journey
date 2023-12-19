a5a95da15eb0a097ac44fdb0ed0c9bc9e3ffe688
DatTran <trancongdat.datct@gmail.com>
2023-12-19 09:31:54 +0700

    add customer validation

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -5,11 +5,11 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
 const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  email: yup.string().email().required(),
-  age: yup.number().positive().integer().required(),
-  password: yup.string().min(4).max(15).required(),
+  firstName: yup.string().required('First name là bắt buộc'),
+  lastName: yup.string().required('Last name là bắt buộc'),
+  email: yup.string().email().required('Email là bắt buộc'),
+  age: yup.number().positive().integer().required('Age là bắt buộc'),
+  password: yup.string().min(4).max(15).required('Password là bắt buộc'),
   confirmPassword: yup.string().oneOf([yup.ref('password'), undefined]),
 })
 
@@ -32,15 +32,21 @@ function MyForm() {
       <div className="inputs">
         <form onSubmit={handleSubmit(handleSubmitform)}>
           <input type="text" {...register('firstName')} placeholder="First Name..." />
+          <p> {errors.firstName?.message} </p>
           <input type="text" {...register('lastName')} placeholder="Last Name..." />
+          <p> {errors.lastName?.message} </p>
           <input type="text" {...register('email')} placeholder="Email..." />
+          <p> {errors.email?.message} </p>
           <input type="text" {...register('age')} placeholder="Age..." />
+          <p> {errors.age?.message} </p>
           <input type="password" {...register('password')} placeholder="Password..." />
+          <p> {errors.password?.message} </p>
           <input
             type="password"
             {...register('confirmPassword')}
             placeholder="Confirm Password..."
           />
+          <p> {errors.confirmPassword?.message} </p>
           <input type="submit" id="submit" />
         </form>
       </div>
